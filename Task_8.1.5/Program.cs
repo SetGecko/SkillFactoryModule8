af56e7ca6ba2d98aeff72b9d75ef9284130abc04
Алексей <setgecko@gmail.com>
2023-03-12 08:52:31 +0300

    Выполнил задание 8.1.5

---
+++ Task_8.1.5/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_8._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Folder folder = new Folder();
+            folder.CreateFolder("FFF");
+        }
+    }
+    public class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+    }
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+    }
+}
