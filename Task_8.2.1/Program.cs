87f9e1cad89f29747bb46cb66c3faea25da95ec8
Алексей <setgecko@gmail.com>
2023-03-12 12:48:15 +0300

    Почти выполнил задание 8.2.1

---
+++ Task_8.2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_8._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateFiles();
+        }
+        static void CalculateFiles()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"D:\\");
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
