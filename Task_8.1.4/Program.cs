27c5a5f2917fd37dc58cfdb3ff3b7e944ddcbe1b
Алексей <setgecko@gmail.com>
2023-03-12 08:46:48 +0300

    Выполнил задание 8.1.4

--- Task_8.1.4/Program.cs
+++ Task_8.1.4/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Drive hdd = new Drive("hdd", 90000, 65000);
+            Console.WriteLine("Свободное место на хдд = {0}",hdd.FreeSpace);            
         }
+    }
+    public class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+
+        }
+
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
     }
 }
