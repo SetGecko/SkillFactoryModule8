e8b79d0037e51996946f85eeec247e62e804ce40
Алексей <setgecko@gmail.com>
2023-03-12 12:43:59 +0300

    Отображение информации о дисках и папках на дисках С

---
+++ ClassesForWorkingWithDisksAndDirectories_8.2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ClassesForWorkingWithDisksAndDirectories_8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // получим системные диски
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            // Пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }
+            Console.WriteLine("-----------------");
+            GetCatalogs();
+        }
+        static void GetCatalogs()
+        {
+            string dirName = @"C:\\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+            }
+        }
+    }
+}
