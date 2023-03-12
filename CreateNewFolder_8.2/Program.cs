ad054e89a72ad2afd6d6e9271bcf57fea8da1832
Алексей <setgecko@gmail.com>
2023-03-12 12:56:37 +0300

    Практика по созданию каталогов

---
+++ CreateNewFolder_8.2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CreateNewFolder_8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"D:\\777");
+            // Объявляем переменную от DirectoryInfo при создании пути в D:\\777
+            // Если путь D:\\777 не существует, то создать
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            // Под D:\\777 создать папку SuperFolderrrrr
+            dirInfo.CreateSubdirectory("SuperFolderrrrr");
+        }
+    }
+}
