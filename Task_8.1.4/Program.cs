79a47e2907d966a4c32e96b2e1361ea9dbd79479
Set Gecko <Set Gecko@DESKTOP-82VM5QH>
2023-03-12 08:37:44 +0300

    Добавьте файлы проекта.

---
+++ Task_8.1.4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_8._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
