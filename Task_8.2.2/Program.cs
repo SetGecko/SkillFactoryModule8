00f9ac5f1877dd0cf71cbc49861b3ff887380d42
Алексей <setgecko@gmail.com>
2023-03-17 09:32:12 +0300

    Выполнил задание 8.2.2

---
+++ Task_8.2.2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_8._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateFiles();
+        }
+        static void CalculateFiles()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"D:\\");
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                }
+
+                DirectoryInfo newDirectory = new DirectoryInfo(@"D:\\newDirectory");
+                if (!newDirectory.Exists)
+                    newDirectory.Create();
+
+                // Получение информации о каталоге
+                Console.WriteLine(newDirectory.GetDirectories().Length + dirInfo.GetFiles().Length);
+                Console.WriteLine($"Название каталога: {newDirectory.Name}");
+                Console.WriteLine($"Полное название каталога: {newDirectory.FullName}");
+                Console.WriteLine($"Время создания каталога: {newDirectory.CreationTime}");
+                Console.WriteLine($"Корневой каталог: {newDirectory.Root}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
