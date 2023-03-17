ad14fce5b333895533a874fc671cc6016d071242
Алексей <setgecko@gmail.com>
2023-03-17 09:44:54 +0300

    Выполинл задание 8.2.4

---
+++ Task_8.2.4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_8._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MoveToTrash();
+        }
+        static void MoveToTrash()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\Users\Set Gecko\Desktop\testFolder");
+                string trashPath = "C:\\$Recycle.Bin\\testFolder";
+
+                dirInfo.MoveTo(trashPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
