8ad967fa0ae18d22aee878419f9729daefdd9829
Алексей <setgecko@gmail.com>
2023-03-18 10:38:48 +0300

    Выполнил задание 8.4.1

---
+++ Task_8.4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_8._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\Set Gecko\\Desktop\\BinaryFile.bin";
+
+            string fileContent;
+
+            using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+            {
+                // Применяем специализированные методы Read для считывания соответствующего типа данных.
+                fileContent = reader.ReadString();
+            }
+
+            Console.Write(fileContent);
+        }
+    }
+}
