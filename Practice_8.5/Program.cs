cd27de79c474f30615a19ce412c34b5e71758a40
Алексей <setgecko@gmail.com>
2023-03-18 12:18:36 +0300

    Практика 8.5

---
+++ Practice_8.5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Practice_8._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader r = new StreamReader(path);
+            Console.WriteLine(r.ReadLine());
+            r.Dispose();
+        }
+    }
+}
